50b7963b675f889f90c13039c4b46074f72459f8
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-01-03 22:14:47 +0900

    post request
    + dev-dependencies
    + req, req.body

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 4000;
 
 app.listen(PORT, function () {
-  console.log(`Listening on PORT: ${PORT}`);
+  console.log(`✅ Listening on PORT: ${PORT}`);
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/write", function (req, res) {
+  res.sendFile(__dirname + "/write.html");
+});
+
 app.get("/pretty", function (req, res) {
   res.send("Pretty Page");
+});
+
+app.post("/add", function (req, res) {
+  console.log(req.body);
+  console.log(req.body.title);
+  console.log(req.body.date);
 });

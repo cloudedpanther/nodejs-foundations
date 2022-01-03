246701e6d98dd6dac2e1389ce8f38ff1311cbad5
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-01-03 20:52:50 +0900

    sendFile()

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.listen(PORT, function () {
   console.log(`Listening on PORT: ${PORT}`);
 });
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
 app.get("/pretty", function (req, res) {
   res.send("Pretty Page");
 });

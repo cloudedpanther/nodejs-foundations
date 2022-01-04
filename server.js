48cfb231387c7dcd0129b711c8cc971950307866
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-01-04 12:42:27 +0900

    communicate with db

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 4000;
 
-app.listen(PORT, function () {
-  console.log(`✅ Listening on PORT: ${PORT}`);
-});
+app.set("view engine", "ejs");
+
+const MongoClient = require("mongodb").MongoClient;
+
+let db;
+
+MongoClient.connect(
+  process.env.DB_URL,
+  { useUnifiedTopology: true },
+  function (err, client) {
+    if (err) console.log(err);
+
+    db = client.db("todoapp");
+
+    app.listen(PORT, function () {
+      console.log(`✅ Listening on PORT: ${PORT}`);
+    });
+  }
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -22,8 +39,21 @@ app.get("/pretty", function (req, res) {
   res.send("Pretty Page");
 });
 
+app.get("/list", function (req, res) {
+  db.collection("post")
+    .find()
+    .toArray(function (err, el) {
+      res.render("list.ejs", { toDos: el });
+    });
+});
+
 app.post("/add", function (req, res) {
-  console.log(req.body);
-  console.log(req.body.title);
-  console.log(req.body.date);
+  const newToDo = {
+    title: req.body.title,
+    date: req.body.date,
+  };
+  db.collection("post").insertOne(newToDo, function (err, el) {
+    console.log("🚀 SAVED!!!");
+  });
+  res.send("Saved");
 });
